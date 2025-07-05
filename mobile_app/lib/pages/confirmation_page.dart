feaee33d620aa0909139e5ccdc9e240edd4cbe77
Akhilesh <akhi.singh1989@gmail.com>
2025-07-05 22:57:58 +1000

    Update confirmation_page.dart

--- mobile_app/lib/pages/confirmation_page.dart
+++ mobile_app/lib/pages/confirmation_page.dart
@@ -1,1 +1,29 @@
+```dart
+import 'package:flutter/material.dart';
+
+class ConfirmationPage extends StatelessWidget {
+  final String incidentType;
+  final String location;
+
+  ConfirmationPage({required this.incidentType, required this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Confirmation')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.check_circle, color: Colors.green, size: 100),
+            Text('Report Submitted!'),
+            Text('Type: $incidentType'),
+            Text('Location: $location'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+```
 
