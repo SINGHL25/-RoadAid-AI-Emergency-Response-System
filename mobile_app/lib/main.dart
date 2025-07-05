becb93b0375f136199b0180eef83ea394f0fefb9
Akhilesh <akhi.singh1989@gmail.com>
2025-07-05 22:56:06 +1000

    Update main.dart

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,1 +1,17 @@
+```dart
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
+void main() => runApp(RoadAidApp());
+
+class RoadAidApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RoadAid AI',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
+}
+```
